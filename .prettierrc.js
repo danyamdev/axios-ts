66e4e72ceb785d18a74247a27f374fe7093a3628
danyamdev <danya.mamin.99@bk.ru>
2023-05-24 10:57:03 +0300

    init app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  singleQuote: true,
+  quoteProps: 'consistent',
+  arrowParens: 'avoid',
+  jsxBracketSameLine: false,
+  endOfLine: 'auto',
+  printWidth: 120,
+  semi: true,
+};
